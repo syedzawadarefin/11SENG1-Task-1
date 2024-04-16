94b50f9041993db18380c05f1c307cd70fff07a1
syedzawadarefin <syed.arefin@education.nsw.gov.au>
2024-04-16 15:16:55 +1000

    for spelling bee or word meanings games, added a function to get any random word from a list of 3000 most commonly used words

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-    
+# memory puzzle - have to remember the order of the 3
+# spelling bee 
+# word meanings (spacy en-core-web-sm module)
+
+import random
+
+words = open("words.txt", 'r')
+wordo = []
+for c in words:
+    wordo.append(c)
+
+print(wordo[random.randint(0, len(wordo))])
