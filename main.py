77a99489ceed744962c2ca0b91fc3abd1bc6bdd5
syedzawadarefin <syed.arefin@education.nsw.gov.au>
2024-04-16 19:50:29 +1000

    created functionality to get random list from word a wordlist, and find the definition of the word

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-# memory puzzle - have to remember the order of the 3
+# memory puzzle
 # spelling bee 
-# word meanings (spacy en-core-web-sm module)
+# word meanings 
 
 import random
+from bs4 import BeautifulSoup
+import requests
 
-words = open("words.txt", 'r')
-wordo = []
-for c in words:
-    wordo.append(c)
+def getWord():
+    global randomword
+    wordslist = open("words.txt", 'r')
+    word = []
+    for c in wordslist:
+        word.append(c)
+    randomword = word[random.randint(0, len(word))]
+    print(randomword)
 
-print(wordo[random.randint(0, len(wordo))])
+def defineWord():
+    website = BeautifulSoup(requests.get("https://dictionary.com/browse/" + randomword).text, features="lxml")
+    definition = str(website.p.get_text())
+    index = definition.index(":")
+    print(definition[0:(index)])
+
+getWord()
+defineWord()
+
+
