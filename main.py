52c2e8cd4dd5db64bf8ca898a1f6e4a627c175c7
syedzawadarefin <syed.arefin@education.nsw.gov.au>
2024-04-27 15:49:48 +1000

    added text to speech functionality (runs whenever program is started but will be fixed later)

--- main.py
+++ main.py
@@ -6,41 +6,53 @@ import customtkinter as tk
 import random
 from bs4 import BeautifulSoup
 import requests
+import gtts
+import playsound
+import os
+#from PIL import Image, ImageTk
 
-class Words():
-    def getWord():
-        global randomword
-        with open("words.txt", 'r') as wordlist:
-            words = []
-            for c in wordlist:
-                words.append(c)
-        randomword = words[random.randint(0, len(words))]
+def getWord():
+    global randomword
+    with open("words.txt", 'r') as wordlist:
+        words = []
+        for c in wordlist:
+            words.append(c)
+    randomword = words[random.randint(0, len(words))]
+    return str(randomword)
+getWord()
+
+
+
+def getWordEasy():
+    global randomword
+    with open("wordseasier.txt", "r") as wordlist:
+        words = []
+        for c in wordlist:
+            words.append(c)
+    randomword = words[random.randint(0, len(words))]
+    return str(randomword)
+
+
+def defineWord():
+    website = BeautifulSoup(requests.get("https://dictionary.com/browse/" + randomword).text, features="lxml")
+    definition = str(website.p.get_text())
+    if definition == "":
+        definition = (website.find_all("p")[1])
+        definition = definition.get_text()
+        if ":" in definition:
+            definition = (definition[0:definition.index(":")])
+            return definition
+    else: 
+        if ":" in definition:
+            return (definition[0:definition.index(":")])
+        else: return definition
         
-
-
-    def getWordEasy():
-        global randomword
-        with open("wordseasier.txt", "r") as wordlist:
-            words = []
-            for c in wordlist:
-                words.append(c)
-        randomword = words[random.randint(0, len(words))]
-
-
-    def defineWord():
-        website = BeautifulSoup(requests.get("https://dictionary.com/browse/" + randomword).text, features="lxml")
-        definition = str(website.p.get_text())
-        if definition == "":
-            definition = (website.find_all("p")[1])
-            definition = definition.get_text()
-            if ":" in definition:
-                definition = (definition[0:definition.index(":")])
-                return definition
-        else: 
-            if ":" in definition:
-                return (definition[0:definition.index(":")])
-            else: return definition
-
+def getAudio(randomword: str):
+     audio = gtts.gTTS(text=randomword)
+     audio.save("audio.mp3")
+     playsound.playsound("audio.mp3")
+     os.remove("audio.mp3")
+getAudio(randomword)
 
 class sidebarMenu(tk.CTkFrame):
     def __init__(self, master):
@@ -48,8 +60,8 @@ class sidebarMenu(tk.CTkFrame):
         self.grid_rowconfigure((4), weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.homebutton = tk.CTkButton(self, text='Home')
-        self.homebutton.grid(padx=10, pady=5, row=0, column=0, sticky="new")
-        self.memorybutton = tk.CTkButton(self, text='Memory Game')
+        self.homebutton.grid(padx=10, pady=(10,5), row=0, column=0, sticky="new")
+        self.memorybutton = tk.CTkButton(self, text='Memory Games')
         self.memorybutton.grid(padx=10, pady=5, row=1, column=0, sticky="new")
         self.spellingbee = tk.CTkButton(self, text='Spelling Bee')
         self.spellingbee.grid(padx=10,row=2, pady=5, column=0, sticky="new")
@@ -62,15 +74,17 @@ class App(tk.CTk):
     def __init__(self):
         super().__init__()
 
+        tk.set_appearance_mode("light")
         self.title("Fun Puzzles")
         self.geometry("1000x700")
-        self.grid_columnconfigure((0,1,2,3), weight=1)
+        self.grid_columnconfigure((1,2,3), weight=1)
         self.grid_rowconfigure((0), weight=1)
 
         self.sidebarMenu = sidebarMenu(self)
-        self.sidebarMenu.grid(column=0, row=0, sticky="nsw")
+        self.sidebarMenu.grid(column=0, row=0, sticky="nsew")
 
-
+        self.button1 = tk.CTkButton(self, text="test")
+        self.button1.grid(column=2, row=0)
 
 app = App()
 app.mainloop()
