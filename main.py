fd177083f6e0030a4cb7ef9a3151880c49f7d933
syedzawadarefin <syed.arefin@education.nsw.gov.au>
2024-05-05 18:24:24 +1000

    spelling bee pre with highscore added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import gtts
 import playsound
+import json
 
 
 root = tk.CTk()
@@ -13,52 +14,109 @@ root.title("English Games")
 root.config(bg="#22223b")
 
 fontlabel = ("Chilanka", 80, "bold")
+fontbtn = ("Chilanka", 40, "bold")
+prefontbtn = "Chilanka", 70, "bold"
+prelabel = ("Chilanka", 100, "bold")
+
+bg = "#22223b"
+tc = "#c9ada7"
+fg = "#4a4e69"
+hover = "#6b729c"
+
+def highscore():
+    global sbscore
+    with open("highscores.json", "r") as file:
+        scores = json.load(file)
+        sbscore = scores["scores"][0]["spellingbee"]
+highscore()
+
+def SpellingbeePre():
+    introlabel.place_forget()
+    spellingbtn.pack_forget()
+    definbtn.pack_forget()
+    quitbtn.pack_forget()
+
+
+    spelling = tk.CTkLabel(root,
+                            text="Spelling Bee",
+                            font=(prelabel),
+                            bg_color=(bg),
+                            text_color=tc)
+    spelling.pack(pady=(110,80))
+
+
+    start = tk.CTkButton(root, 
+                         text="Play",
+                         width=350,
+                         height=125,
+                         font=prefontbtn,
+                         fg_color=fg,
+                         bg_color=bg,
+                         hover_color=hover,
+                         corner_radius=7,
+                         border_width=5,
+                         border_color="white",
+                         command=SpellingBee)
+    start.pack()
+    
+    highscore = tk.CTkLabel(root,
+                            text="2",
+                            font=("Chilanka", 30, "bold"),
+                            bg_color=bg,
+                            text_color=tc)
+    highscore.pack(pady=50)
+
+    highscore.configure(text=f"High Score: {sbscore}")
+
+def SpellingBee():
+    pass
 
 introlabel = tk.CTkLabel(root, 
                             text="English Games",
-                            font=("Chilanka", 80, "bold"),
-                            bg_color="#22223b",
-                            text_color="#c9ada7")
+                            font=(fontlabel),
+                            bg_color=bg,
+                            text_color=tc)
 #introlabel.pack()
-introlabel.place(relx = 0.5, rely=0.25, anchor = tk.CENTER)
+introlabel.place(relx = 0.5, rely=0.2, anchor = tk.CENTER)
 
 spellingbtn = tk.CTkButton(root, 
                             text="Spelling Bee", 
                             width=350, 
                             height=125, 
-                            font=("Chilanka", 40, "bold"),
-                            fg_color=("#4a4e69"),
-                            hover_color="#6b729c",
+                            font=(fontbtn),
+                            fg_color=(fg),
+                            hover_color=hover,
                             corner_radius=7,
                             border_width=5,
                             border_color="white",
-                            bg_color="#22223b")
+                            bg_color=bg,
+                            command=SpellingbeePre)
 spellingbtn.pack(pady=(250,0))
 
 definbtn = tk.CTkButton(root, 
                             text="Definitions", 
                             width=350, 
                             height=125, 
-                            font=("Chilanka", 40, "bold"),
-                            fg_color="#4a4e69",
-                            hover_color="#6b729c",
+                            font=(fontbtn),
+                            fg_color=fg,
+                            hover_color=hover,
                             corner_radius=7,
                             border_width=5,
                             border_color="white",
-                            bg_color="#22223b")
+                            bg_color=bg)
 definbtn.pack(pady=(35,0))
 
 quitbtn = tk.CTkButton(root, 
                             text="Quit", 
                             width=150, 
                             height=40, 
-                            font=("Chilanka", 25, "bold"),
-                            fg_color="#4a4e69",
-                            hover_color="#d60000",
+                            font=(fontbtn),
+                            fg_color=fg,
+                            hover_color=hover,
                             corner_radius=5,
                             border_width=5,
                             border_color="white",
-                            bg_color="#22223b")
+                            bg_color=bg)
 quitbtn.pack(pady=(35,0))
 
 
